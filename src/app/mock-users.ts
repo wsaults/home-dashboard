818a05c33087faff7923f64ba3a3360c83daaef2
Will Saults <wsaults@yahoo.com>
2018-08-26 19:01:09 -0500

    Displaying a list of family members and details

---
+++ src/app/mock-users.ts
@@ -0,0 +1,8 @@
+import { User } from './user';
+
+export const USERS: User[] = [
+  { id: 1, name: 'Dad' },
+  { id: 2, name: 'Marci' },
+  { id: 3, name: 'Isaac' },
+  { id: 4, name: 'Zander' }
+];
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { USERS } from '../mock-users';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  users = USERS;
+  selectedUser: User;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelect(user: User): void {
+    this.selectedUser = user;
+  }
+}
